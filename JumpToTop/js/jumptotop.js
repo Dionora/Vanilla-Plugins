837fb69ca0f9e12423a7332fc6c2d0267702b68b
VrijVlinder <adalib@mac.com>
2014-03-14 19:03:27 -0600

    Remodeled a few things
    I made these changes to get it to work better for me

--- JumpToTop/js/jumptotop.js
+++ JumpToTop/js/jumptotop.js
@@ -1,22 +1,33 @@
 /* Copyright 2013 Zachary Doll */
-$(function(){
-	// store element for performance.
-	$('#JumpToTop').hide();
+jQuery(document).ready(function($) {
+  // Fade in.out on scroll
+  $(window).scroll($.debounce(function() {
+    if ($(this).scrollTop() > 100) {
+      $('#JumpToTop').fadeIn();
+    } else {
+      $('#JumpToTop').fadeOut();
+    }
 	
-	// Fade in.out on scroll
-	$(function () {
-		$(window).scroll(function () {
-			if ($(this).scrollTop() > 100) {
-				$('#JumpToTop').fadeIn();
-			} else {
-				$('#JumpToTop').fadeOut();
-			}
-		});
-		 
-		// scroll body to 0px on click
-		$('#JumpToTop a').click(function (event) {
-			event.preventDefault();
-			$('body, html').animate({scrollTop: 0}, 800);
-		});
-	});
+	var ScrollOffset = $(document).height() - $(window).height() - 100;
+	console.log(ScrollOffset);
+	console.log($(this).scrollTop());
+	if ($(this).scrollTop() > ScrollOffset) {
+      $('#JumpToBottom').fadeOut();
+    } else {
+      $('#JumpToBottom').fadeIn();
+    }
+  }, 100, null, true, true));
+
+  // scroll body to 0px on click
+  $('#JumpToTop').click(function(event) {
+    event.preventDefault();
+    $('body, html').animate({scrollTop: 0}, 800);
+  });
+  
+  // scroll body to bottom of page on click
+  $('#JumpToBottom').click(function(event) {
+    event.preventDefault();
+	var ScrollOffset = $(document).height() - $(window).height();
+    $('body, html').animate({scrollTop: ScrollOffset}, 800);
+  });  
 });
---
+++ JumpToTop/jttop.js
@@ -0,0 +1,9 @@
+ $(document).ready(function() {
+
+$('#Body').prepend('<a href="#" title="To Bottom"><div id="JumpToBottom"></div></a>');
+$('#Foot').append('<div class="Bottom"></div>');
+$('.Bottom').prepend('<a href="#" title="To Top"><div id="JumpToTop"></div></a>');
+
+
+
+});
